edb5585ca9bec519df2d28f92f141067ab3e27d0
juniorsolo <junior.solo@hotmail.com>
2017-08-07 21:44:58 -0300

    Reference metod

--- src/br/com/alura/OrdenaStrings.java
+++ src/br/com/alura/OrdenaStrings.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class OrdenaStrings {
 
@@ -22,9 +23,19 @@ public class OrdenaStrings {
 		//Collections.sort(palavras, ordena);
 		
 		//palavras.sort( (s1, s2) -> Integer.compare(s1.length(), s2.length() ));
-		palavras.sort((s1, s2) -> s1.length() - s2.length());
+		//palavras.sort((s1, s2) -> s1.length() - s2.length());
 		
-		//onsumer<String> impressor = s-> System.out.println(s);
+		//função q recebe uma string e retorna o length dela
+		Function<String, Integer> funcao = String::length;
+		Function<String, Integer> funcao2 = s -> s.length();
+ 		
+		
+		palavras.sort(Comparator.comparing(s -> s.length() ));
+		//ou
+		palavras.sort(Comparator.comparing(String::length));
+		
+		
+		Consumer<String> impressor = s-> System.out.println(s);
 		//palavras.forEach(impressor);
 		
 		// Lambda com consumer embutido
@@ -32,6 +43,7 @@ public class OrdenaStrings {
 		
 		//ou
 		palavras.forEach(s -> System.out.println(s) );
+		palavras.forEach(System.out::println );
 		
 
 	}
